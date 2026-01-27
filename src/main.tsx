8817755af326bbac6ff959115073add7c4e3dfb7
ngocnht <ngocnht@ghn.vn>
2026-01-27 23:06:12 +0700

    Remove project name from package.json; import layout styles in main.tsx; clean up global.scss by removing layout import

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { ConfigProvider } from 'antd'
 import 'antd/dist/reset.css'
 import '@/styles/global.scss'
+import '@/styles/layout.scss'
 import '@/i18n'
 import App from './App'
 
